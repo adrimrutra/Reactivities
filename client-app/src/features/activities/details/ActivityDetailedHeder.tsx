c53d733bbb88a9effe0d8191839312428aaa10aa
Artur Mirda <adrimrutra@gmail.com>
2020-01-31 13:24:32 +0200

    Upload Widget, React Dropzone, React cropper

--- client-app/src/features/activities/details/ActivityDetailedHeder.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeder.tsx
@@ -1,55 +1,69 @@
-import React from 'react'
-import { Segment, Item, Header, Button, Image } from 'semantic-ui-react'
+import React from 'react';
+import { Segment, Item, Header, Button, Image } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 
-
 const activityImageStyle = {
-    filter: 'brightness(30%)'
-  };
-  
-  const activityImageTextStyle = {
-    position: 'absolute',
-    bottom: '5%',
-    left: '5%',
-    width: '100%',
-    height: 'auto',
-    color: 'white'
-  };
+  filter: 'brightness(30%)'
+};
 
-const ActivityDetailedHeder: React.FC<{activity: IActivity}> = ({activity}) => {
-    return (
-        <Segment.Group>
-        <Segment basic attached='top' style={{ padding: '0' }}>
-          <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid  style={activityImageStyle}/>
-          <Segment basic style={activityImageTextStyle}>
-            <Item.Group>
-              <Item>
-                <Item.Content>
-                  <Header
-                    size='huge'
-                    content={activity.title}
-                    style={{ color: 'white' }}
-                  />
-                  <p>{activity.date}</p>
-                  <p>
-                    Hosted by <strong>Bob</strong>
-                  </p>
-                </Item.Content>
-              </Item>
-            </Item.Group>
-          </Segment>
-        </Segment>
-        <Segment clearing attached='bottom'>
-          <Button color='teal'>Join Activity</Button>
-          <Button>Cancel attendance</Button>
-          <Button as={Link} to={`/manage/${activity.id}`} color='orange' floated='right'>
-            Manage Event
-          </Button>
-        </Segment>
-      </Segment.Group>
-    )
-}
+const activityImageTextStyle = {
+  position: 'absolute',
+  bottom: '5%',
+  left: '5%',
+  width: '100%',
+  height: 'auto',
+  color: 'white'
+};
 
-export default observer(ActivityDetailedHeder)
+const ActivityDetailedHeder: React.FC<{ activity: IActivity }> = ({
+  activity
+}) => {
+  const host = activity.attendees.filter(x => x.isHost)[0];
+  return (
+    <Segment.Group>
+      <Segment basic attached="top" style={{ padding: '0' }}>
+        <Image
+          src={`/assets/categoryImages/${activity.category}.jpg`}
+          fluid
+          style={activityImageStyle}
+        />
+        <Segment basic style={activityImageTextStyle}>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content={activity.title}
+                  style={{ color: 'white' }}
+                />
+                <p>{activity.date}</p>
+                <p>
+                  Hosted by{' '}
+                  <Link to={`/profile/${host.username}`}>
+                    <strong>{host.displayName}</strong>
+                  </Link>
+                </p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      </Segment>
+      <Segment clearing attached="bottom">
+        <Button color="teal">Join Activity</Button>
+        <Button>Cancel attendance</Button>
+        <Button
+          as={Link}
+          to={`/manage/${activity.id}`}
+          color="orange"
+          floated="right"
+        >
+          Manage Event
+        </Button>
+      </Segment>
+    </Segment.Group>
+  );
+};
+
+export default observer(ActivityDetailedHeder);

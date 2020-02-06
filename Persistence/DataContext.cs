c443fd79db8926127760e7dd2a538a67615dcbb5
Artur Mirda <adrimrutra@gmail.com>
2020-02-06 12:03:57 +0200

    Clean

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,13 +6,10 @@ namespace Persistence
 {
     public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
+        public DataContext(DbContextOptions options) : base(options) {}
 
-        public DbSet<Value> Values { get; set; }
+        
         public DbSet<Activity> Activities { get; set; }
-
         public DbSet<UserActivity> UserActivities { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Comment> Comments { get; set; }
@@ -21,13 +18,6 @@ namespace Persistence
         {
             // alows at migration give to AppUser key as a string
             base.OnModelCreating(builder);
-
-            builder.Entity<Value>()
-                .HasData(
-                    new Value { Id = 1, Name = "Value 101" },
-                    new Value { Id = 2, Name = "Value 102" },
-                    new Value { Id = 3, Name = "Value 103" }
-                );
 
             builder.Entity<UserActivity>(x => x.HasKey(ua =>
                 new { ua.AppUserId, ua.ActivityId }));
